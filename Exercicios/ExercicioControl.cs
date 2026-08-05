0db5f239359d73f407813fe324af00df04e4feec
GUSTAVO BRITO BOLETTI <gustavo.bboletti@senacsp.edu.br>
2026-08-05 11:37:28 -0300

    segundo commit

---
+++ Exercicios/ExercicioControl.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class ExercicioControl
+    {
+        Exercicio_model modelo;//Conectando a Classe Model e Control
+        private int opcao;
+
+        public ExercicioControl()
+        {
+            this.modelo = new Exercicio_model();
+            SetOpcao(0);
+        }//Conastrutor Padrão
+
+       
+        private int GetOpcao()
+        {
+            return this.opcao;
+        }//fim do GetOpção
+        public void SetOpcao(int opcao)
+        {
+            this.opcao = opcao;
+        }//fim do SetOpção
+
+        public void Coletar()
+        {
+            //Pegue os dois
+            Console.Write("Informe o primeiro número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Informe o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Vou passar para o reconstrutor com parâmetro
+            this.modelo = new Exercicio_model(num1, num2);
+        }//fim do coletar = Pega os dois números digitados
+
+        public void ColetarReal()
+        {
+            //Pegar dois números
+            Console.WriteLine("Informe o primeiro número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            //Chamar o construtor que recebe reais 
+            this.modelo = new Exercicio_model(num1, num2);
+            //fim do coletar real
+        }
+
+        //Mostrar as opções de operação na tela
+        public void menu()
+        {
+            Console.WriteLine("\nEscolha uma das opções abaixo: \n" +
+                              "0. Sair\n"                         +
+                              "1. Soma\n"                         +
+                              "2. Subtração\n"                    +
+                              "3. Divisão\n"                      +
+                              "4. Multiplicação\n"                +
+                              "5. Potência\n"                     +
+                              "6. Potência com Parâmetros\n"      +
+                              "7. Raiz\n"                         +
+                              "8. Raiz com Parâmetros\n");
+            SetOpcao(Convert.ToInt32(Console.ReadLine()));
+        }//fim do menu
+
+        //Executar o menu
+
+        public void Executarmenu()
+        {
+            do
+            {
+                menu();//mostrar as opções
+                switch (GetOpcao())
+                {
+                    case 0:
+                        Console.WriteLine("Obrigado!");
+                        break;
+
+                    case 1:
+                        Coletar();//Coletar os dois números
+                        Console.WriteLine("A soma é " + this.modelo.Soma());//Fazer a operação de soma
+                        break;//Finalizar o case
+                    case 2:
+                        Coletar();
+                        Console.WriteLine("A subtração é " + this.modelo.Subtrair());
+                        break;
+                    case 3:
+                        Coletar();
+                        //Validação
+                        if (this.modelo.Dividir() == -1)
+                        {
+                            Console.WriteLine("Impossivel de dividir!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("A divisão é " + this.modelo.Dividir());
+                        }
+                        break;
+                    case 4:
+                        Coletar();
+                        Console.WriteLine("A multiplicação é: " + this.modelo.Multiplicar());
+                        break;
+                    case 5:
+                        ColetarReal();
+                        Console.WriteLine("A potência é: " + this.modelo.Potencia());
+                        break;
+                    case 6:
+                        Console.WriteLine("Informe a base: ");
+                        double bas = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe o expoente: ");
+                        double expoente = Convert.ToDouble(Console.ReadLine());
+                        //Mostrar o resultado da operação
+                        Console.WriteLine("A potência é: " + this.modelo.Potencia(bas, expoente));
+                        break;
+                    case 7:
+                        ColetarReal();
+                        Console.WriteLine("A Raiz do primeiro número é: " + this.modelo.Raiznum3());
+                        Console.WriteLine("A Raiz do segundo número é: " + this.modelo.Raiznum4());
+                        break;
+                    case 8:
+                        Console.WriteLine("Informe o primeiro número: ");
+                        double num1 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe o segundo número: ");
+                        double num2 = Convert.ToDouble(Console.ReadLine());
+                        //Chamar o método que calcula a Raiz
+                        Console.WriteLine("A Raiz do primeiro número é: " + this.modelo.Raiz(num1));
+                        Console.WriteLine("A raiz do segundo número é: " + this.modelo.Raiz(num2));
+                        break;
+                    default:
+                        Console.WriteLine("A opção escolhida não é válida!");
+                        break;
+                }//fim da escolha
+            } while (GetOpcao() != 0);//fim...while
+        }//fim do método executar menu
+
+    }//fim da classe
+}//fim do projeto
---
+++ Exercicios/Exercicio_model.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class Exercicio_model
+    {
+        //Área para declaração de variáveis globais
+        private int num1;
+        private int num2;
+        private double num3;
+        private double num4;
+        private int resultado;
+
+        //Método Construtor: Instância as variáveis na memória
+
+        public Exercicio_model()
+        {
+            Setnum1(0);
+            Setnum2(0);
+            SetResultado(0);
+        }//fim do método construtor
+
+        public Exercicio_model(int num1, int num2)
+        {
+            Setnum1(num1);
+            Setnum2(num2);
+            SetResultado(0);
+        }//fim do construtor
+
+        public Exercicio_model(double num3,double num4)
+        {
+            Setnum3(num3);
+            Setnum4(num4);
+        }//fim do Getnum
+
+        //métodos GETS e SETS
+        //GET = Pegar, obter, Consultar
+        //SET = Alterar, Modificar, Configurar
+
+        public int Getnum1()
+        {
+            return this.num1;
+        }//fim do getNum1
+
+        public void Setnum1(int num1)
+        {
+            this.num1 = num1;
+        }//fim do setNum1
+
+        public int Getnum2()
+        {
+            return this.num2;
+        }//fim do getNum2
+
+        public void Setnum2(int num2)
+        {
+            this.num2 = num2;
+        }//fim do setNum2
+
+        public int GetResultado()
+        {
+            return this.resultado;
+        }//fim do método resultado
+
+        public double Getnum3()
+        {
+            return this.num3;
+        }//fim do getnum3
+
+        public void Setnum3(double num3)
+        {
+            this.num3 = num3;
+        }//fim do Setnum3
+
+        public double Getnum4()
+        {
+            return this.num4;
+        }//fim do Getnum 4
+
+        public void Setnum4(double num4)
+        {
+            this.num4 = num4;
+        }//fim do Setnum4
+
+        public void SetResultado(int resultado)
+        {
+            this.resultado = resultado;
+        }//fim do método resultado
+
+        public int Soma()
+        {
+            return this.num1 + this.num2;
+        }//fim do método somar
+
+        public int Subtrair()
+        {
+            SetResultado(Getnum1() - Getnum2());
+            return GetResultado();
+        }//fim do método Subtrair
+
+        public int Multiplicar()
+        {
+            SetResultado(Getnum1() * Getnum2());
+            return GetResultado();
+        }//fim do método multiplicar
+
+        public double Dividir()
+        {
+            if (Getnum2() == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                SetResultado(Getnum1() / Getnum2());
+                return GetResultado();
+            }//fim do if
+        }//fim do método dividir
+
+        public double Potencial(double bas, double expoente)
+        {
+            return Math.Pow(bas, expoente);
+        }//fim da potencia
+
+        public double Raiz(double num)
+        {
+            return Math.Sqrt(num);
+        }//fim da raiz
+
+        public double Potencia()
+        {
+            return Math.Pow(Getnum3(), Getnum4());
+        }//fim da potência sem parâmetro 
+
+        public double Potencia(double bas, double expoente)
+        {
+            return Math.Pow(bas, expoente);
+        }//fim da potência sem parâmetro 
+
+
+        public double Raiznum3()
+        {
+            return Raiz(Getnum3());
+        }//fim da Raiz
+
+        public double Raiznum4()
+        {
+            return Raiz(Getnum4());
+        }//fim da Raiz
+
+        
+
+    }//fim da classe
+}//fim do projeto
